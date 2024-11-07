6812b002dc39529c1ef42da12cbcd412459c95ae
wengshian94 <wsho3@illinois.edu>
2024-11-06 21:46:38 -0500

    added spacy

--- code/process/process_data.py
+++ code/process/process_data.py
@@ -1,11 +1,20 @@
 import pandas as pd
-import numpy as np
-
+import os
+import re
+import spacy
+from spacy.language import Language
+from spacy_langdetect import LanguageDetector
 
 class DataProcessor:
 
     """
-    Data class to fetch raw data and process it
+    Data class to fetch raw data and process it.
+
+    Sample call:
+
+    #Dump the processed file into  the processed data folder
+    data_processor = DataProcessor()
+    processed_data = data_processor.process_data(dump_processed = True)
     """
 
     def __init__(self):
@@ -16,35 +25,49 @@ class DataProcessor:
         Fetch raw data
         """
         biden_tweets = pd.read_csv('../../data/train/raw/hashtag_joebiden.csv', encoding_errors='ignore', lineterminator='\n')
+        biden_tweets['candidate'] = 'other'
         trump_tweets = pd.read_csv('../../data/train/raw/hashtag_donaldtrump.csv', encoding_errors='ignore', lineterminator='\n')
+        trump_tweets['candidate'] = 'trump'
         all_tweets = pd.concat([biden_tweets, trump_tweets])
         all_tweets = all_tweets.drop_duplicates('tweet_id')
         all_tweets['timestamp'] = pd.to_datetime(all_tweets['created_at'])
+        
         self.df_data = all_tweets
 
     def process_data(self,
-                     only_US: bool,
-                     only_english: bool):
+                     dump_processed: bool,
+                     processed_filename: str = 'processed_data'):
         """
         Filter and process raw data
-        Args:
-            only_US: True to only filter data coming from US
-            only_english: True to only filter data where tweets are in English
+
+        dump_processed: True to dump processed file
+        processed_filename: Name of processed file
         
         Returns:
             df_processed: Dataframe with 2 columns: i) timestamp ii) textdata
         """
         
+        #Load and label the data
+        nlp = spacy.load('en_core_web_sm')  # 1
+        def create_lang_detector(nlp, name):
+            return LanguageDetector()
+        Language.factory("language_detector", func=create_lang_detector)
+        nlp.add_pipe('language_detector', last=True)
+        self.df_data['is_en'] = self.df_data['tweet'].apply(lambda x: self.check_en(x, nlp))
+        
         df_processed = self.df_data.copy()
         df_processed = df_processed.rename(columns = {'tweet': 'textdata'})
         df_processed['textdata'] = df_processed['textdata'].apply(self.clean_text)
-        if only_US:
-            df_processed = df_processed.loc[df_processed['country'] == 'United States of America']
         
-        if only_english:
-            pass
-        
-        df_processed = df_processed[['timestamp', 'textdata']]
+        if dump_processed:
+            #Check if file dir exists
+            filedir = '../../data/train/processed/'
+            if not os.path.exists(filedir):
+                os.makedirs(filedir)
+            filename = os.path.join(filedir, f'{processed_filename}.csv')
+            #dump file
+            df_processed.to_csv(filename)
+            print(f'File dump into: {filename}')
         return df_processed
 
     @staticmethod
@@ -53,6 +76,16 @@ class DataProcessor:
         text = re.sub(r'\@\w+|\#','', text)  # Remove mentions and hashtags
         text = re.sub(r"[^a-zA-Z0-9\s]", '', text)  # Remove special characters
         return text.lower().strip()  # Convert to lowercase and strip extra spaces
+
+    @staticmethod
+    def check_en(txt_data, nlp_obj):
+        doc = nlp_obj(txt_data) #3
+        detect_language = doc._.language #4
+        return detect_language.get('language', None) == 'en'
+
+
+
+
         
 
         
